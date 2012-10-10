2164122865f560572d71fef2d42619d15c447471
Eric Waters <ewaters@shutterstock.com>
2012-10-09 21:37:32 -0400

    Add a typedef for u31

--- thrift/demo.thrift
+++ thrift/demo.thrift
@@ -1,12 +1,18 @@
 namespace * DemoAPI
 
+/*
+	Define an unsigned 31 bit int
+	@validate range 0-
+*/
+typedef i32 u31
+
 service Calculator {
 	/*
-		Add two numbers
+		Add two positive numbers
 		@rest GET /add/:number_a/:number_b
 	*/
-	i32 add (
-		1: i32 number_a,
-		2: i32 number_b
+	u31 add (
+		1: u31 number_a,
+		2: u31 number_b
 	)
 }
