c426eac7bacc49cb03ce9ce690c83d5199ee2fd5
Eric Waters <ewaters@shutterstock.com>
2012-10-09 21:26:20 -0400

    Install the Tapir handler to the Dancer app

--- lib/Demo.pm
+++ lib/Demo.pm
@@ -1,5 +1,10 @@
 package Demo;
 use Dancer ':syntax';
+use Dancer::Plugin::Tapir;
+
+setup_tapir_handler
+    thrift_idl => 'thrift/demo.thrift',
+    handler_class => 'Calculator';
 
 our $VERSION = '0.1';
 
